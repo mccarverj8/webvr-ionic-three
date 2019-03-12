be899c48eb39d432f683b8777cb15d55214aae3e
James M <mccarverj8@gmail.com>
2019-03-12 11:21:53 -0500

    first commit

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-page-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HomePage {
+
+}
